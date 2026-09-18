a871e026cb3d673c287ea625c21d71c4501deb9c
rinat <nasyrov-rin.nasyrov@yandex.ru>
2026-09-18 09:42:15 +0400

    Возвращаю ответ через tcp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rusty_redis::*;
-use std::{io::BufReader, net::TcpListener};
+use std::io::Write;
+use std::thread;
+use std::{io::BufReader, net::TcpListener, time::Duration};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -10,18 +12,29 @@ fn main() {
         // Пока обрабатываем все в основном потоке, потом вынесем в отдельные
         let mut reader = BufReader::new(&stream);
         loop {
-            execute_command(read_resp_messenge(&mut reader));
+            let response = execute_command(read_resp_messenge(&mut reader));
+            // Здесь прикол чтобы обойти ограничение сосуществование
+            // мутабельной и иммутабельной ссылок, вызываем метод на
+            // иммутабельной ссылке, для которой реализован трейт Write
+            // как для самого типа
+            let write_result = (&stream).write_all(response.as_bytes());
+            println!("{write_result:?}")
         }
     }
 }
-fn execute_command(input: RESPMessenge) {
+fn execute_command(input: RESPMessenge) -> String {
     let plain_command = input.get_plain_command();
     // Матчим именно срез потому, что со срезами матчинг работает нормально,
     // а сложные объекты с литералами сопоставлять не может, тк в расте не принято
     // чтобы встроенные операторы языка делали неявные преобразования
     match plain_command[..] {
-        ["PING"] => println!("PONG"),
-        ["COMMAND", "HELP"] => println!("Подробная справка"),
-        _ => println!("Неизвестная комманда"),
+        ["PING"] => "+PONG\r\n".to_string(),
+        ["PING30"] => {
+            // Долгий запрос для теста праллельной/ассинхронной обработки
+            thread::sleep(Duration::from_secs(30));
+            "+PONG30\r\n".to_string()
+        }
+        ["COMMAND", "HELP"] => "Подробная справка\r\n".to_string(),
+        _ => "-Неизвестная комманда\r\n".to_string(),
     }
 }
